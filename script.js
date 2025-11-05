d368b2aef3b365b22c44e813cdeb077efd753496
Surjeet Kumar <157746293+surjeetkumar8006@users.noreply.github.com>
2025-11-05 20:39:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
-  
-  module.exports = debounce;
+  let timer = null;
+
+  return function (...args) {
+    const context = this;
+    const callNow = immediate && !timer;
+
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      timer = null;
+      if (!immediate) {
+        callback.apply(context, args);
+      }
+    }, delay);
+
+    if (callNow) {
+      callback.apply(context, args);
+    }
+  };
+}
+
+module.exports = debounce;
